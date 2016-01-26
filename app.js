7d60abf16ad1137b1b4976461507c319159b2b87
jayzabs <dczii@live.com>
2016-01-26 12:34:59 +0800

    Custom Thread Test
    I have created only 1 page for this test but I have added Approve/Reject
    selection before the bookings will be made

---
+++ app.js
@@ -0,0 +1,79 @@
+// create our angular module and inject firebase
+angular.module('scheduleApp', ['firebase'])
+
+// create our main controller and get access to firebase
+.controller('mainController', function($scope, $firebase) {
+  
+  // our application code will go here
+
+		// connect to firebase 
+		var ref = new Firebase("https://customtest.firebaseio.com/days");  
+		var fb = $firebase(ref);
+
+		// sync as object 
+	  		var syncObject = fb.$asObject();
+	  	// three way data binding
+	    	syncObject.$bindTo($scope, 'days');
+
+	    //uncheck all checkbox
+			    $scope.x = function () {
+		        $scope.approve=false;
+		        $scope.approve[0].enabled=true;
+		    	}
+
+		// function to set the default data
+			  $scope.reset = function() {    
+
+			    fb.$set({
+			      monday: {
+			        name: 'Monday',
+			        id: 1,
+			        slots: {
+			          0900: {
+			            time: '9:00am',
+			            booked: false,
+			            id: 1
+			          },
+			          0110: {
+			            time: '11:00am',
+			            booked: false,
+			            id: 2
+			          }
+			        }
+			      },
+			      tuesday: {
+			        name: 'Tuesday',
+			        id: 2,
+			        slots: {
+			          0900: {
+			            time: '9:00am',
+			            booked: false
+			          },
+			          0110: {
+			            time: '11:00am',
+			            booked: false
+			          }
+			        }
+			      },
+			      wednesday: {
+			        name: 'Wednesday',
+			        slots: {
+			          0900: {
+			            time: '9:00am',
+			            booked: false
+			          },
+			          0110: {
+			            time: '11:00am',
+			            booked: false
+			          }
+			        }
+			      },
+			    });    
+
+
+			    
+			  };
+
+			  
+});
+
